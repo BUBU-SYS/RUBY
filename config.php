b891d0182b37822e47b5b75836c09c85c55dd7ba
BUBU-SYS <rudagijv85@gmail.com>
2026-02-17 07:42:03 +0100

    Add files via upload

--- config.php
+++ config.php
@@ -1,28 +1,30 @@
 <?php
-	//Site url - add / at the end, eg: http://metin2cms.cf/mt2/ IMPORTANT!!!
-	$site_url = "https://ameria.to";
-	
-	//Game database
-	$host = "94.23.77.86";
-	$user = "wbuser853";
-	$password = "DAkugehdAKG!hds";
-	
-	//Mail settings
-	$SMTPAuth = true;
-	$SMTPSecure = "ssl";
-	$EmailHost = "ameria.to";
-	$emailPort = 465;
-	
-	$email_username = "contact@ameria.to";//gmail account
-	$email_password = "1221212112";//gmail password
-	
-	//Register
-	$safebox_size = 1;
+// TEST config (zadne realne pristupy)
 
+// Site url - s lomitkem na konci pokud to tvuj web vyzaduje
+$site_url = "http://localhost:8000/";
 
-	$captchakey = "6LeboCkqAAAAAPD-r7SsOnp95_Fc6T-XQ2k1y-jR";
-	$secretkey = "6LeboCkqAAAAAG9Ytze8PK3ilJl51i4NPzv1576d";
-	
-	$item_id = 60302;
-	$item_count = 1;
-	
+// Game database (FAKE)
+$host = "127.0.0.1";
+$user = "test_user";
+$password = "test_password";
+
+// Mail settings (FAKE)
+$SMTPAuth = false;
+$SMTPSecure = "ssl";
+$EmailHost = "localhost";
+$emailPort = 465;
+
+$email_username = "test@example.com";
+$email_password = "test_password";
+
+// Register
+$safebox_size = 1;
+
+// reCAPTCHA (FAKE)
+$captchakey = "TEST_SITE_KEY";
+$secretkey  = "TEST_SECRET_KEY";
+
+// Item reward (pokud to web vyzaduje)
+$item_id = 60302;
+$item_count = 1;
