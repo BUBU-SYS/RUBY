7fdf2d84710ab912a3ff28fb3873059c07490724
BUBU-SYS <rudagijv85@gmail.com>
2026-02-17 07:20:29 +0100

    Add files via upload

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+	//Site url - add / at the end, eg: http://metin2cms.cf/mt2/ IMPORTANT!!!
+	$site_url = "https://ameria.to";
+	
+	//Game database
+	$host = "94.23.77.86";
+	$user = "wbuser853";
+	$password = "DAkugehdAKG!hds";
+	
+	//Mail settings
+	$SMTPAuth = true;
+	$SMTPSecure = "ssl";
+	$EmailHost = "ameria.to";
+	$emailPort = 465;
+	
+	$email_username = "contact@ameria.to";//gmail account
+	$email_password = "1221212112";//gmail password
+	
+	//Register
+	$safebox_size = 1;
+
+
+	$captchakey = "6LeboCkqAAAAAPD-r7SsOnp95_Fc6T-XQ2k1y-jR";
+	$secretkey = "6LeboCkqAAAAAG9Ytze8PK3ilJl51i4NPzv1576d";
+	
+	$item_id = 60302;
+	$item_count = 1;
+	
