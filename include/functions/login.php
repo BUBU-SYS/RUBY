3858bd38edba9c9af4041e806271a7b2304be743
BUBU-SYS <rudagijv85@gmail.com>
2026-06-25 17:46:32 +0200

    Delete include directory

--- include/functions/login.php
+++
@@ -1,9 +0,0 @@
-<?php
-	if(isset($_POST['username']) && isset($_POST['password']))
-	{
-		$username = strip_tags($_POST['username']);
-		$password = strip_tags($_POST['password']);
-		
-		$login_info = $database->doLogin($username,$password);
-	}
-?>
